cfa26c80ae620dee447040b34219b8e2f2fd3381
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-08 15:03:31 +0545

    add routes and server PORT

--- class-4/backend/app.js
+++ class-4/backend/app.js
@@ -4,7 +4,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("SAAS DEVELOPMENT")
+});
+
+app.get("/videos", (req, res) => {
+    
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`SERVER IS RUNNING ON PORT: ${3000}`);
-})
+});
+
+console.log("backend development");
