0fd17998d06388bdf5f012542ded3b3700b78f69
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-11 14:27:51 +0545

    write sql database connection logic

--- Book-Management-System/app.js
+++ Book-Management-System/app.js
@@ -1,4 +1,6 @@
+dotenv.config();
 import express from 'express';
+import dotenv from 'dotenv'
 const app = express();
 
 app.use(express.json());
--- Book-Management-System/db/db.js
+++ Book-Management-System/db/db.js
@@ -1,2 +1,21 @@
 import { Sequelize, DataTypes } from "sequelize"
+import { DB_URL } from "../utils/env.js";
+import { SELECT } from "sequelize/lib/query-types";
+const sequelize = new Sequelize(DB_URL);
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("database connected successfully")
+    }).catch((err) => {
+        console.log("error", err.stack)
+    })
+
+const db = {};
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+const db = {
+    Sequelize : sequelize,
+    sequelize: sequelize
+};
 
