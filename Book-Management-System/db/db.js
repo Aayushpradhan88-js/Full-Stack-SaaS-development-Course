fdab2984d03947f469e6b1b221125b10f66cc7ec
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-12 15:30:39 +0545

    add environmental sample file

--- Book-Management-System/db/db.js
+++ Book-Management-System/db/db.js
@@ -2,7 +2,6 @@ const {DataTypes, Sequelize} = require("sequelize");
 const {DB_URL} = require("../utils/env.js");
 
 const sequelize = new Sequelize(DB_URL);
-console.log("sequelize", DB_URL)
 
 sequelize.authenticate()
     .then(() => {
