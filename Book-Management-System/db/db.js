620d815aea57d6b4c89d62b11938080045ada2a8
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-12 14:42:16 +0545

    database migration best practices

--- Book-Management-System/db/db.js
+++ Book-Management-System/db/db.js
@@ -21,13 +21,36 @@ const db = { // db bhadoo maa create gareko model haru save hunxa
 // db.book = require('../models/book.model')(sequelize, DataTypes)
 
 const bookModel = require('../models/book.model.js');
-const res = bookModel(sequelize, DataTypes);
-db.book = res;
+const bookRes = bookModel(sequelize, DataTypes);
+db.book = bookRes;
+
+const userModel = require('../models/user.model.js');
+const userRes = userModel(sequelize, DataTypes);
+db.user = userRes
 
 //---MIGRATION DATABASE---//
-sequelize.sync({force:false})
+
+/*
+@force:false - update the database and delete the all data of column
+*/
+// sequelize.sync({force:false}) 
+// .then(() => {
+//     console.log("database migration successful");
+// })
+
+
+/*
+@alter:false - only update the column name
+*/
+sequelize.sync({alter:false}) 
 .then(() => {
-    console.log("database migration successful");
+    console.log("Database is successfully migrated");
 })
+
+// if(migration){
+//     console.log("Database is successfully migrated");
+// } else {
+//     console.error("failed to migrate the database", error.stack);
+// }
 
 module.exports = {db}
