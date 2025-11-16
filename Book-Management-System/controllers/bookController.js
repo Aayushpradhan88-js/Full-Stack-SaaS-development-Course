a79b376858bf687285b532437e65e2efd63d63a1
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-16 13:52:55 +0545

    fix: logical error post api

--- Book-Management-System/controllers/bookController.js
+++ Book-Management-System/controllers/bookController.js
@@ -24,7 +24,7 @@ const getAllBooks = async (_, res) => {
 const postBook = async (req, res) => {
     const { bookname, bookprice, bookauthor, bookgeneric } = req.body;
     if (
-        [bookname, bookprice, bookauthor, bookgeneric].some((fields) => fields.trim() === '')
+        [bookname, bookprice, bookauthor, bookgeneric].some((fields) => fields.trim === '')
     ) {
         return res.status(401).json({
             success: false,
@@ -40,31 +40,39 @@ const postBook = async (req, res) => {
         });
     };
 
-    if (10 < bookprice) {
+    if (10 > bookprice) {
         return res.status(402).json({
             success: false,
             message: 'Enter at least RS.10 then above'
         });
     };
 
-    const createBook = await db.books.create({
-        bookname,
-        bookprice,
-        bookauthor,
-        bookgeneric
-    });
-    if (!createBook) {
-        return res.status(400).json({
-            success: false,
-            message: 'failed to upload book in database',
+    try {
+        const createBook = await db.books.create({
+            bookname,
+            bookprice,
+            bookauthor,
+            bookgeneric
         });
-    };
+        if (!createBook) {
+            return res.status(400).json({
+                success: false,
+                message: 'failed to upload book in database',
+            });
+        };
 
-    return res.status(200).json({
-        success: true,
-        message: 'successfully upload book in database',
-        data: createBook
-    });
+        return res.status(200).json({
+            success: true,
+            message: 'successfully upload book in database',
+            data: createBook
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'internal server error',
+            error: error.stack
+        });
+    }
 };
 
 module.exports = { getAllBooks, postBook }
