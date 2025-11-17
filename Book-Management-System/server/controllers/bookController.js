dcda6e793f80bf4aa680a7aab9301810149fcd56
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-17 11:44:30 +0545

    shift code base to server for API creation and instlled vite for writing frontend

--- Book-Management-System/server/controllers/bookController.js
+++ Book-Management-System/server/controllers/bookController.js
@@ -109,6 +109,8 @@ const postBook = async (req, res) => {
 };
 
 const editBookDetails = async (req, res) => {
+    const { bookname, bookprice, bookauthor, bookgeneric } = req.body;
+    const minPriceValue = 50;
     const urlId = req.params.id;
     if (!urlId) {
         return res.json({
@@ -124,9 +126,25 @@ const editBookDetails = async (req, res) => {
         });
     };
 
+    if (bookname && bookname === fetchBooks.bookname) {
+        const sameBook = await db.books.findOne(bookname);
+        if (sameBook) {
+            return res.status(405).json({
+                message: "book name is already registered"
+            })
+        }
+    };
+    if(minPriceValue < bookprice || bookprice === ''){
+        return res.status(402).json({
+            message: `${bookprice} is very low price`
+        })
+    }
+
+    //@TODO: IF BOOK PRICE IS HIGH THEN 
+
     const updateBook = db.books.update({
         bookname, bookprice, bookauthor, bookgeneric
-    }, { where: { id } });
+    }, { where: { id: userId } });
     if (!updateBook) {
         return res.status(403).json({
             success: false,
@@ -134,14 +152,27 @@ const editBookDetails = async (req, res) => {
         });
     };
 
-    const { bookauthor } = updateBook;
+    const bookauthorname = updateBook.bookauthor;
 
     return res.status(200).json({
         success: true,
-        message: `${bookauthor} you're successfully updated`,
+        message: `${bookauthorname} you're successfully updated`,
         data: updateBook
     });
 
 }
 
-module.exports = { getAllBooks, postBook, getAllBooksById,editBookDetails }
+const deleteBook = async (req, res) => {
+    const urlId = req.params.id; //first we fetch the data
+    if (!urlId) {
+        return res.status(402).json({
+            success: false,
+            message: "cannot get url"
+        })
+    }
+
+    const existingData = await db.books.findByPk(urlId);
+
+}
+
+module.exports = { getAllBooks, postBook, getAllBooksById, editBookDetails }
--- Book-Management-System/server/routes/bookRoutes.js
+++ Book-Management-System/server/routes/bookRoutes.js
@@ -4,6 +4,7 @@ const bookRouter = require('express').Router();
 const { getAllBooks,getAllBooksById, postBook, editBookDetails } = require('../controllers/bookController');
 
 bookRouter.route('/').get(getAllBooks).post(postBook);
-bookRouter.route('/:id').get(getAllBooksById).patch(editBookDetails).delete(deleteBook);
+bookRouter.route('/:id').get(getAllBooksById).patch(editBookDetails)
+// .delete(deleteBook);
 
 module.exports = bookRouter;
