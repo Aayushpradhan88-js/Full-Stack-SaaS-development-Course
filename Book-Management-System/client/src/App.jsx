3452c1ee2fcec044a6e1d9bf5b41f46f89d03203
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-22 13:36:26 +0545

    add react router dom

--- Book-Management-System/client/src/App.jsx
+++ Book-Management-System/client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// JSX yeutaa markdown language hoo react le banako hoina. JSX yeuta HTML jastaa hoo. Yo yeutaa opensource library hoo jun react le integrate gareko matra hoo
 
-  return (
+//Component - component le JSX return garxa yedi return gardaaina bhane tio function ho yaa tq aru kehi kura hoo tra JSX hoina.
+function App() { //App() - component ko naam hoo
+  // return bhanda bahiraa lekhinee code JavaScript code hoo jaha logic huru hunxa
+  let name = "aayush";
+
+
+  return ( //UI ma kehi kura dekhaunu xa bhane return ma lekhinxaa JSX ani return bhitra JS ko logic lekhnaa mildaainaa
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>
+      Hello 
+      {name} 
+      {/* { } - JavaScript ko variable access garnaa ko lagi yesari garnee, calculation garnaa ko lagi */}
+      </h1>
     </>
   )
 }
+
+//function
+function app(a, b) {
+  return a+b
+}
+
+app(1,2);
 
 export default App
---
+++ Book-Management-System/client/src/docs/React-note.md
@@ -0,0 +1,22 @@
+# DAY-17
+
+`````````
+Casing - variable lekhnee approach
+Types
+   LowerCase : isexisted
+   UpperCase : ISEXISTED
+   CamelCase (JS developer ho bhane praya sabaai engineer lee yo case follow garxan) isExisted
+   PascalCase : Isexisted (JAVA, C# language heavily used)
+   SnakeCase : is_existed (python language heavily used)
+   KebabCase : is-existed (worlds laai connect garnaa bich bataa aar paar garnaa)
+
+So casing kina important xa throughout the project bhane - praya project ma yo approach follow garda dheriaai think garnu pardaaina kin maile kun case maa tio variable set gareko thiyeee.....yeslee error raa time save garxaa
+````````
+
+````````
+Routing
+
+React ma routing hudaainaa, uslee tio feature provide gardaainaa haamilee bahiraa bata launue parxa means package installation garnu parxaa. kina provide na garekoo? kina bhanee react autaa library hoo matlab dheraai kura Uslee provide gardaaina......
+
+Package ko naam  - react-router-dom
+````````
