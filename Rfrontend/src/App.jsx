ff2829878c72b964f840e87349d5ef901b8eb6bf
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-24 14:03:27 +0545

    add react router dom in frontend

--- Rfrontend/src/App.jsx
+++ Rfrontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'   
-import Effect from './hooks/effect'
+import Effect from './hooks/Effect'
 
 function App() {
     return(
