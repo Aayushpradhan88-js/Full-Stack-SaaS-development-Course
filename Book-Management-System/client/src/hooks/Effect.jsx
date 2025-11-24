9499216382dd39f855bee61b7bbdf70245e24ad2
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-24 13:52:43 +0545

    useEffect concept finished

--- Book-Management-System/client/src/hooks/Effect.jsx
+++ Book-Management-System/client/src/hooks/Effect.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Effect() {
+    const [number1, setNumber1] = useState(0);
+    const increasingNumber = () => {
+        setNumber1(number1 + 1);
+    };
 
-    // useEffect(() => {
-
-    // }, [])
-    console.log(useEffect);
+    useEffect(() => {
+        console.log("Number increased!!");
+    }, [number1])
 
     return (
+        <>
+        <h1>{number1}</h1>
+        <button onClick={increasingNumber}>click me</button>
         <h1>understanding use effect hook</h1>
+        </>
     )
 };
 
