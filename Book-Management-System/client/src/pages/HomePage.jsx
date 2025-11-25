b2cf9498f014ff38170d6cdfe577ed27945ee5a2
Aayush <AAYUSHPRADHAN996@outlook.com>
2025-11-25 14:11:10 +0545

    add button component for re-usability

--- Book-Management-System/client/src/pages/HomePage.jsx
+++ Book-Management-System/client/src/pages/HomePage.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
+import Button from '../components/Button'
 
 const HomePage = () => {
   return (
     <>
       <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
-        <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+        <div class="flex flex-wrap items-center justify-between max-w-screen-7xl px-4 mx-auto">
           <a href="#" class="flex items-center">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Book Management</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Book Management System</span>
           </a>
           <div class="flex items-center lg:order-2">
             <div class="hidden mt-2 mr-4 sm:inline-block">
               <span></span>
             </div>
 
-            <a href="https://themesberg.com/product/tailwind-css/landing-page"
-              class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Create Book</a>
+            <Button title="Create" />
             <button data-collapse-toggle="mobile-menu-2" type="button"
               class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="mobile-menu-2" aria-expanded="true">
